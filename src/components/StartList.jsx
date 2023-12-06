1d1fa6983f4bf178c63e739c9994cd45e4bc08b6
Kushnierova <Katerynakushnierova@gmail.com>
2023-12-06 21:21:55 +0100

    21:21

--- src/components/StartList.jsx
+++ src/components/StartList.jsx
@@ -1,11 +1,11 @@
 // import PropTypes from 'prop-types';
-import { Name} from './Name/Name';
-import css from './Name.module.css';
+// import { Name} from './Name/Name';
+// import css from './Name.module.css';
 
-export const Name = () => {
-  return (
-  );
-};
+// export const Name = () => {
+//   return (
+//   );
+// };
 
 // Name.propTypes = {
 //   friends: PropTypes.array.isRequired,
