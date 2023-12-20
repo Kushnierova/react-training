3380ae0e916a88879c4435e91f6f05f26463daac
Kushnierova <Katerynakushnierova@gmail.com>
2023-12-20 15:08:18 +0100

    15:08

---
+++ src/components/ThirdLesson/Reader/Reader.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Reader extends Component {
+  state = {
+    publicationIndex: 0,
+  };
+  render(){
+    return(
+        <div>fdhbrfhe</div>
+    )
+  }
+}
+
+export default Reader;
--- src/components/ThirdLesson/ThirdLesson.jsx
+++ src/components/ThirdLesson/ThirdLesson.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import css from './ThirdLesson.module.css'
+import css from './ThirdLesson.module.css';
 import VideoList from './VideoList';
 import Player from './Player';
 import videos from './video.json';
+import Reader from './Reader';
 
 class ThirdLesson extends Component {
   state = {
@@ -15,10 +16,17 @@ class ThirdLesson extends Component {
 
   render() {
     return (
-      <div className={css.containerPlayer} style={{ padding: 24 }}>
-        <h1 className={css.titlePlayer}>Selected video: {this.state.selectedVideo}</h1>
-        <VideoList videos={videos} onSelect={this.selectVideo} />
-        <Player url={this.state.selectedVideo} />
+      <div>
+        <div className={css.containerPlayer} style={{ padding: 24 }}>
+          <h1 className={css.titlePlayer}>
+            Selected video: {this.state.selectedVideo}
+          </h1>
+          <VideoList videos={videos} onSelect={this.selectVideo} />
+          <Player url={this.state.selectedVideo} />
+        </div>
+        <div>
+          <Reader />
+        </div>
       </div>
     );
   }
