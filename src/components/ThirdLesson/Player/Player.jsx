b6b472cafa63de660ba0d7eddc8781039b89a9e0
Kushnierova <Katerynakushnierova@gmail.com>
2024-01-05 19:25:04 +0100

    19:24

--- src/components/ThirdLesson/Player/Player.jsx
+++ src/components/ThirdLesson/Player/Player.jsx
@@ -1,41 +1,70 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { PlayerWrapper, StyledPlayer } from './Player.styled';
 
-class Player extends Component {
-  state = {
-    isVideoLoaded: false,
-  };
+function Player({ url }) {
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+  const showLoader = url && !isVideoLoaded;
+  const playerSize = isVideoLoaded ? '100%' : 0;
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.url !== this.props.url) {
-      // console.log('New url');
-      this.setState({ isVideoLoaded: false });
+  useEffect(() => {
+    if (!url) {
+      setIsVideoLoaded(false);
     }
-  }
+  }, [url]);
 
-  render() {
-    const { isVideoLoaded } = this.state;
-    const { url } = this.props;
-    const showLoader = url && !isVideoLoaded;
-    const playerSize = isVideoLoaded ? '100%' : 0;
-
-    return (
-      <>
-        {showLoader && <h2>Відео завантажується...</h2>}
-        {url && (
-          <PlayerWrapper>
-            <StyledPlayer
-              url={url}
-              width={playerSize}
-              height={playerSize}
-              onReady={() => this.setState({ isVideoLoaded: true })}
-              controls
-            />
-          </PlayerWrapper>
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      {showLoader && <h2>Відео завантажується...</h2>}
+      {url && (
+        <PlayerWrapper>
+          <StyledPlayer
+            url={url}
+            width={playerSize}
+            height={playerSize}
+            onReady={() => setIsVideoLoaded(true)}
+            controls
+          />
+        </PlayerWrapper>
+      )}
+    </>
+  );
 }
+
+// class OldPlayer extends Component {
+//   state = {
+//     isVideoLoaded: false,
+//   };
+
+//   componentDidUpdate(prevProps) {
+//     if (prevProps.url !== this.props.url) {
+//       // console.log('New url');
+//       this.setState({ isVideoLoaded: false });
+//     }
+//   }
+
+//   render() {
+//     const { isVideoLoaded } = this.state;
+//     const { url } = this.props;
+//     const showLoader = url && !isVideoLoaded;
+//     const playerSize = isVideoLoaded ? '100%' : 0;
+
+//     return (
+//       <>
+//         {showLoader && <h2>Відео завантажується...</h2>}
+//         {url && (
+//           <PlayerWrapper>
+//             <StyledPlayer
+//               url={url}
+//               width={playerSize}
+//               height={playerSize}
+//               onReady={() => this.setState({ isVideoLoaded: true })}
+//               controls
+//             />
+//           </PlayerWrapper>
+//         )}
+//       </>
+//     );
+//   }
+// }
 
 export default Player;
--- src/components/ThirdLesson/Reader/Reader.jsx
+++ src/components/ThirdLesson/Reader/Reader.jsx
@@ -1,75 +1,104 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Controls from './Controls';
 import Progress from './Progress';
 import Publication from './Publication/Publication';
 
 const LS_KEY = 'reader_item_index';
 
-class Reader extends Component {
-  state = {
-    index: 0,
+function Reader({ items }) {
+  const [index, setIndex] = useState(0);
+  const totalItems = items.length;
+  const currentItem = items[index];
+
+  const changeIndex = value => {
+    setIndex(index => index + value);
   };
 
-  // Тут я придумую як переключати сторынки так щоб пысля 12 йшла 1, а до 1 йшла 12
-  //   checkChangeIndex = value => {
-  //     const index = this.state.index;
-  //     const totalItems = this.props.items.length;
-  //     // console.log(index)
-  //     // console.log(totalItems);
-  //     {
-  //       index > totalItems || index < 0
-  //         ? this.setState({ [index]: totalItems })
-  //         : this.setState(state => ({ index: state.index + value }));
-  //     }
-  //     //     if (index < totalItems || index > 0) {
-  //     //       this.setState(state => ({ index: state.index + value }));
-  //     //     }
-  //     //     // else if (index > totalItems) {
-  //     //     //   this.setState({ index: '0' });
-  //     //     // }
-  //     //     else {
-  //     //       this.setState({ index : totalItems });
-  //     //     }
-  //   };
+  // useEffect(() => {
+  //   const savedState = Number(localStorage.getItem(items[index]));
+  //   console.log(savedState);
+  //   if (savedState) {
+  //     setIndex(savedState);
+  //   }
+  //   if (!index) {
+  //     localStorage.setItem(LS_KEY, index);
+  //   }
+  // }, [ items,index]);
 
-  changeIndex = value => {
-    this.setState(state => ({ index: state.index + value }));
-  };
-
-  //   Якщо людина перший раз заходить на сайт,і в LocalStorage нічого немає(null) тоді впаде сайт,
-  //  щоб цього не сталось ми робимо наступне
-  componentDidMount() {
-    // console.log(localStorage.getItem(LS_KEY));
-    const savedState = localStorage.getItem(LS_KEY);
-    // if(savedState:true) null=false
-    if (savedState) {
-      this.setState({ index: Number(savedState) });
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.index !== this.state.index) {
-      localStorage.setItem(LS_KEY, this.state.index);
-    }
-  }
-
-  render() {
-    // console.log(this.props.items[this.state.index]);
-    const { index } = this.state;
-    const { items } = this.props;
-    const totalItems = items.length;
-    const currentItem = items[index];
-    return (
-      <div>
-        <Controls
-          onChange={this.changeIndex}
-          current={index + 1}
-          total={totalItems}
-        />
-        <Progress current={index + 1} total={totalItems} />
-        <Publication item={currentItem} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Controls onChange={changeIndex} current={index + 1} total={totalItems} />
+      <Progress current={index + 1} total={totalItems} />
+      <Publication item={currentItem} />
+    </div>
+  );
 }
+
+// class Reader extends Component {
+//   state = {
+//     index: 0,
+//   };
+
+//   // Тут я придумую як переключати сторынки так щоб пысля 12 йшла 1, а до 1 йшла 12
+//   //   checkChangeIndex = value => {
+//   //     const index = this.state.index;
+//   //     const totalItems = this.props.items.length;
+//   //     // console.log(index)
+//   //     // console.log(totalItems);
+//   //     {
+//   //       index > totalItems || index < 0
+//   //         ? this.setState({ [index]: totalItems })
+//   //         : this.setState(state => ({ index: state.index + value }));
+//   //     }
+//   //     //     if (index < totalItems || index > 0) {
+//   //     //       this.setState(state => ({ index: state.index + value }));
+//   //     //     }
+//   //     //     // else if (index > totalItems) {
+//   //     //     //   this.setState({ index: '0' });
+//   //     //     // }
+//   //     //     else {
+//   //     //       this.setState({ index : totalItems });
+//   //     //     }
+//   //   };
+
+//   changeIndex = value => {
+//     this.setState(state => ({ index: state.index + value }));
+//   };
+
+//   //   Якщо людина перший раз заходить на сайт,і в LocalStorage нічого немає(null) тоді впаде сайт,
+//   //  щоб цього не сталось ми робимо наступне
+//   componentDidMount() {
+//     // console.log(localStorage.getItem(LS_KEY));
+//     const savedState = localStorage.getItem(LS_KEY);
+//     // if(savedState:true) null=false
+//     if (savedState) {
+//       this.setState({ index: Number(savedState) });
+//     }
+//   }
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.index !== this.state.index) {
+//       localStorage.setItem(LS_KEY, this.state.index);
+//     }
+//   }
+
+//   render() {
+//     // console.log(this.props.items[this.state.index]);
+//     const { index } = this.state;
+//     const { items } = this.props;
+//     const totalItems = items.length;
+//     const currentItem = items[index];
+//     return (
+//       <div>
+//         <Controls
+//           onChange={this.changeIndex}
+//           current={index + 1}
+//           total={totalItems}
+//         />
+//         <Progress current={index + 1} total={totalItems} />
+//         <Publication item={currentItem} />
+//       </div>
+//     );
+//   }
+// }
 
 export default Reader;
